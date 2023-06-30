96dd3f371db0fc95dd21e3ddea60db3cfab63671
Bad1m <96011877+Bad1m@users.noreply.github.com>
2023-06-30 13:05:26 +0300

    client folder transfer

--- DistributionSystemApi/DistributionSystemApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DistributionSystemApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
